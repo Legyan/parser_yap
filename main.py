9c4898c318117248abab7b5d694d8342c20207f8
Levon Gegamyan <levongeg@gmail.com>
2023-02-07 23:01:08 +0300

    prepared parser, no logs

---
+++ main.py
@@ -0,0 +1,108 @@
+import re
+from urllib.parse import urljoin
+
+import requests_cache
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+from configs import configure_argument_parser
+from constants import BASE_DIR, MAIN_DOC_URL
+from outputs import control_output
+
+
+def whats_new(session):
+    whats_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
+
+    response = session.get(whats_new_url)
+    response.encoding = 'utf-8'
+
+    soup = BeautifulSoup(response.text, features='lxml')
+
+    main_div = soup.find('section', attrs={'id': 'what-s-new-in-python'})
+    div_with_ul = main_div.find('div', attrs={'class': 'toctree-wrapper'})
+    sections_by_python = div_with_ul.find_all(
+        'li', attrs={'class': 'toctree-l1'}
+        )
+
+    results = [('Ссылка на статью', 'Заголовок', 'Редактор, автор')]
+    for section in tqdm(sections_by_python):
+        version_a_tag = section.find('a')
+        href = version_a_tag['href']
+        version_link = urljoin(whats_new_url, href)
+        response = session.get(version_link)
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, features='lxml')
+        h1 = soup.find('h1')
+        dl = soup.find('dl')
+        dl_text = dl.text.replace('\n', ' ')
+        results.append((version_link, h1.text, dl_text))
+    return results
+
+
+def latest_versions(session):
+    response = session.get(MAIN_DOC_URL)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'lxml')
+    sidebar = soup.find('div', attrs={'class': 'sphinxsidebarwrapper'})
+    ul_tags = sidebar.find_all('ul')
+    for ul in ul_tags:
+        if 'All versions' in ul.text:
+            a_tags = ul.find_all('a')
+            break
+    else:
+        raise Exception('Ничего не нашлось')
+    pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
+    results = [('Ссылка на документацию', 'Версия', 'Статус')]
+    for tag in a_tags:
+        link = (tag['href'])
+        text_match = re.search(pattern, tag.text)
+        if text_match:
+            version, status = text_match.group('version', 'status')
+            results.append((link, version, status))
+        else:
+            results.append((link, tag.text, ''))
+    return results
+
+
+def download(session):
+    # Вместо константы DOWNLOADS_URL, используйте переменную downloads_url.
+    downloads_url = urljoin(MAIN_DOC_URL, 'download.html')
+    response = session.get(downloads_url)
+    soup = BeautifulSoup(response.text, features='lxml')
+    table = soup.find('table', {'class': 'docutils'})
+    pattern = r'.+pdf-a4\.zip$'
+    pdf_a4_tag = table.find('a', {'href': re.compile(pattern)})
+    pdf_a4_link = pdf_a4_tag['href']
+    archive_url = urljoin(downloads_url, pdf_a4_link)
+    archive_response = session.get(archive_url)
+    filename = archive_url.split('/')[-1]
+    downloads_dir = BASE_DIR / 'downloads'
+    downloads_dir.mkdir(exist_ok=True)
+    archive_path = downloads_dir / filename
+    with open(archive_path, 'wb') as file:
+        file.write(archive_response.content)
+
+
+MODE_TO_FUNCTION = {
+    'whats-new': whats_new,
+    'latest-versions': latest_versions,
+    'download': download,
+}
+
+
+def main():
+    arg_parser = configure_argument_parser(MODE_TO_FUNCTION.keys())
+    args = arg_parser.parse_args()
+    session = requests_cache.CachedSession()
+
+    if args.clear_cache:
+        session.cache.clear()
+
+    parser_mode = args.mode
+    results = MODE_TO_FUNCTION[parser_mode](session)
+    if results:
+        control_output(results, args)
+
+
+if __name__ == '__main__':
+    main()
